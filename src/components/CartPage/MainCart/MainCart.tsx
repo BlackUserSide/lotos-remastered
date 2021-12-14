5130f733ec967c0b0024f086dd112a7aebf2f803
BlackUserSide <nesmserg@gmail.com>
2021-12-14 14:20:07 +0200

    fix cart sale

--- src/components/CartPage/MainCart/MainCart.tsx
+++ src/components/CartPage/MainCart/MainCart.tsx
@@ -175,7 +175,7 @@ export const MainCart: React.FC = () => {
         if (tmpLocal) {
           let newSum = 0;
 
-          if (tmpPrice > 1200) {
+          if (tmpPrice - (tmpPrice * 20) / 100 >= 1200) {
             setDataSales((prev) => ({
               ...prev,
               secondProcent: true,
